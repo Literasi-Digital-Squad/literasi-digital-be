7291d7864cc484464c612329d3cbb53d951a93a7
tanto <tantows001@gmail.com>
2025-03-25 07:36:00 +0700

    feat(ErrMiddleware): add status on error middleware

--- src/middleware/error-middleware.ts
+++ src/middleware/error-middleware.ts
@@ -1,20 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 import { ZodError } from "zod";
 import { ResponseErorr } from "../error/reponse-error";
+import { Status } from "../lib/constant";
 
 export const errorMiddleware = async (error: Error, req: Request, res: Response, next: NextFunction) => {
     if (error instanceof ZodError) {
         res.status(400).json({
+            status: Status.Error,
             error: `Validation error : ${JSON.stringify(error)}`
         })
     }
     else if (error instanceof ResponseErorr) {
         res.status(error.status).json({
+            status: Status.Error,
             error: error.message
         })
     }
     else {
         res.status(500).json({
+            status: Status.Error,
             error: error.message
         })
     }
