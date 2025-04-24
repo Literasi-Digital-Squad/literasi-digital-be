fd1d73a089e2288a53f45300e68843392d0e1a26
tanto <tantows001@gmail.com>
2025-04-24 12:36:52 +0700

    feat(seeder): seeder setup core admin question seeder

---
+++ prisma/seeders/admin-seeder.ts
@@ -0,0 +1,12 @@
+export const admin = [
+    {
+        "name": "admin 1",
+        "password": "$2a$10$qL0NV2P51RvE4SN1LY8Xze9j7Yksqc8Me3t8Tmn38jWEBLNVsi7pq",
+        "email": "admin1@gmail.com"
+    },
+    {
+        "name": "admin 2",
+        "password": "$2a$10$qL0NV2P51RvE4SN1LY8Xze9j7Yksqc8Me3t8Tmn38jWEBLNVsi7pq",
+        "email": "admin2@gmail.com"
+    },
+]
